cd9bf81589846aedc3cc6304eb1845b8d3da4c0f
RohanGautam <rohan1gautam@gmail.com>
2019-12-10 12:08:57 +0800

    rough start/stop implementation

--- joystickMouse.py
+++ joystickMouse.py
@@ -4,11 +4,15 @@ from time import time
 # pyautogui.moveRel(0, 100, duration=5)
 
 start = pyautogui.position()
+previous = start
+counter = 1
 while True:
     # print(pyautogui.position())
     current = pyautogui.position()
-    previous = start
 
-    if current!=previous:
-        print("Started")
-
+    if current != previous:
+        print("Started") # TODO : show once when started, show "ended" once when ended.
+    elif current == previous and counter%2000==0:
+        print("Ended")
+    previous = current
+    counter+=1
