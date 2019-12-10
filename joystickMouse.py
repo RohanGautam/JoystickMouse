760554063ef64960d5f6d69a6510176aa31b1253
RohanGautam <rohan1gautam@gmail.com>
2019-12-10 12:22:41 +0800

    minor code refactoring

--- joystickMouse.py
+++ joystickMouse.py
@@ -5,20 +5,19 @@ from time import time
 
 start = pyautogui.position()
 previous = start
-counter = 1
-startCount = 0
-endCount = 0
+counter, startCount, endCount = 0, 0, 0
+
 while True:
-    # print(pyautogui.position())
     current = pyautogui.position()
 
     if (current != previous) and (startCount == 0):
-        print("Started") 
-        startCount+=1
+        print("Started")
+        startCount += 1
         endCount = 0
-    elif (current == previous) and (counter%2000 == 0) and (endCount==0): # counter%2000==0 to intruduce some delay. make this dependant on time maybe
+    # counter%2000==0 to intruduce some delay. make this dependant on time maybe
+    elif (current == previous) and (counter % 2000 == 0) and (endCount == 0):
         print("Ended")
         startCount = 0
-        endCount+=1
+        endCount += 1
     previous = current
-    counter+=1
+    counter += 1
