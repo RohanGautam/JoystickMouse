e354aecc2b57648486516e3753ef82106e75f585
RohanGautam <rohan1gautam@gmail.com>
2019-12-10 12:19:58 +0800

    improved start/stop detector

--- joystickMouse.py
+++ joystickMouse.py
@@ -6,13 +6,19 @@ from time import time
 start = pyautogui.position()
 previous = start
 counter = 1
+startCount = 0
+endCount = 0
 while True:
     # print(pyautogui.position())
     current = pyautogui.position()
 
-    if current != previous:
-        print("Started") # TODO : show once when started, show "ended" once when ended.
-    elif current == previous and counter%2000==0:
+    if (current != previous) and (startCount == 0):
+        print("Started") 
+        startCount+=1
+        endCount = 0
+    elif (current == previous) and (counter%2000 == 0) and (endCount==0): # counter%2000==0 to intruduce some delay. make this dependant on time maybe
         print("Ended")
+        startCount = 0
+        endCount+=1
     previous = current
     counter+=1
